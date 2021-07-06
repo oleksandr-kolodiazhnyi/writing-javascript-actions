9ca912a2cb6b6d4640554039c9df6ae5a4998194
Oleksandr Kolodiazhnyi <oleksandr.kolodiazhny@takeoff.com>
2021-07-06 17:56:23 +0300

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+if (thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`);
+}
