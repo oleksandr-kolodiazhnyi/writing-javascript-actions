16f43f1322ec69d5c0914c5493d50c7c23260cf1
Oleksandr Kolodiazhnyi <oleksandr.kolodiazhny@takeoff.com>
2021-07-06 18:08:16 +0300

    fix errors

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,3 +1,5 @@
+const core = require("@actions/core");
+
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
